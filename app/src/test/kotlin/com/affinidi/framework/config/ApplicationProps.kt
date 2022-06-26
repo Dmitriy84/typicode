37c07a370b30f9f403a286c14874f7efe6a7f021
Dmytro Malohlovets <DMYTROMA@ext.inditex.com>
2022-06-26 20:30:36 +0300

    finished

--- app/src/test/kotlin/com/affinidi/framework/config/ApplicationProps.kt
+++ app/src/test/kotlin/com/affinidi/framework/config/ApplicationProps.kt
@@ -8,10 +8,4 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConfigurationProperties(prefix = "application")
 class ApplicationProps {
     lateinit var baseURI: String
-    lateinit var users: List<User>
-
-    class User {
-        lateinit var email: String
-        lateinit var password: String
-    }
 }
